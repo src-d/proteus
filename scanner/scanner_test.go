dfe9612781c789f0434b69f32062239b7a7b92b5
Miguel Molina <hi@mvader.me>
2016-12-13 18:14:45 +0100

    Tests for processing types

--- scanner/scanner_test.go
+++ scanner/scanner_test.go
@@ -1,6 +1,8 @@
 package scanner
 
 import (
+	"go/token"
+	"go/types"
 	"os"
 	"path/filepath"
 	"testing"
@@ -37,4 +39,83 @@ func TestParseSourceFiles(t *testing.T) {
 	require.Nil(t, err)
 
 	require.Equal(t, "foo", pkg.Name())
+}
+
+func TestProcessType(t *testing.T) {
+	cases := []struct {
+		name     string
+		typ      types.Type
+		expected Type
+	}{
+		{
+			"named type",
+			newNamed("/foo/bar", "Bar", nil),
+			NewNamed("/foo/bar", "Bar"),
+		},
+		{
+			"basic type",
+			types.Typ[types.Int],
+			NewBasic("int"),
+		},
+		{
+			"basic array",
+			types.NewArray(types.Typ[types.Int], 8),
+			repeated(NewBasic("int")),
+		},
+		{
+			"basic slice",
+			types.NewSlice(types.Typ[types.Int]),
+			repeated(NewBasic("int")),
+		},
+		{
+			"basic behind a pointer",
+			types.NewPointer(types.Typ[types.Int]),
+			NewBasic("int"),
+		},
+		{
+			"named behind a pointer",
+			types.NewPointer(newNamed("/foo/bar", "Bar", nil)),
+			NewNamed("/foo/bar", "Bar"),
+		},
+		{
+			"map of basic and named",
+			types.NewMap(
+				types.Typ[types.Int],
+				newNamed("/foo/bar", "Bar", nil),
+			),
+			NewMap(
+				NewBasic("int"),
+				NewNamed("/foo/bar", "Bar"),
+			),
+		},
+		{
+			"struct",
+			types.NewStruct(nil, nil),
+			nil,
+		},
+		{
+			"interface",
+			types.NewInterface(nil, nil),
+			nil,
+		},
+	}
+
+	for _, c := range cases {
+		require.Equal(t, c.expected, processType(c.typ), c.name)
+	}
+}
+
+func repeated(t Type) Type {
+	t.SetRepeated(true)
+	return t
+}
+
+func newNamed(path, name string, underlying types.Type) types.Type {
+	obj := types.NewTypeName(
+		token.NoPos,
+		types.NewPackage(path, "mock"),
+		name,
+		underlying,
+	)
+	return types.NewNamed(obj, underlying, nil)
 }
