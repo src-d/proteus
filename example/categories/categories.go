e2c9e79727d819245c9485e798daaba6fe3ccc59
Miguel Molina <hi@mvader.me>
2017-05-10 15:29:31 +0200

    add proto folder path to proto_path

---
+++ example/categories/categories.go
@@ -0,0 +1,8 @@
+package categories
+
+// CategoryOptions is not marked for generations, but it is used in another
+// structs, so it will be generated because of it.
+type CategoryOptions struct {
+	ShowPrices bool
+	CanBuy     bool
+}
--- example/server.proteus.go
+++ example/server.proteus.go
@@ -31,12 +31,6 @@ func (s *exampleServiceServer) GetPhone(ctx context.Context, in *GetPhoneRequest
 	result = GetPhone()
 	return
 }
-func (s *exampleServiceServer) RandomCategory(ctx context.Context, in *RandomCategoryRequest) (result *CategoryOptions, err error) {
-	result = new(CategoryOptions)
-	aux := RandomCategory()
-	result = &aux
-	return
-}
 func (s *exampleServiceServer) RandomNumber(ctx context.Context, in *RandomNumberRequest) (result *RandomNumberResponse, err error) {
 	result = new(RandomNumberResponse)
 	result.Result1 = RandomNumber(in.Arg1, in.Arg2)
